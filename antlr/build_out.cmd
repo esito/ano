88b96bd80129103919b9a03f8554acda4343a6b6
Martin Holøs <mh@esito.no>
2021-04-28 18:29:28 +0200

    minor fix to build_out.cmd

--- antlr/build_out.cmd
+++ antlr/build_out.cmd
@@ -1,8 +1,10 @@
 copy "./index.ts" "./out/parser" /Y
 copy "./package_out.json" "./out/parser" /Y
 cd out/parser 
+del "package.json"
 ren "package_out.json" "package.json"
-cd ../..
+cd ..
+cd ..
 call npm run compile
 robocopy ".\out\parser" "..\vscode\src\parser" *.ts /MIR
 call echo %CD%
