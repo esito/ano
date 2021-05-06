de6238409ce23a096fcba931d7933a81380704a5
Per Digre <per@digre.com>
2021-05-06 17:07:13 +0200

    Noe galt men nærmere

--- vscode/src/backend/completion.ts
+++ vscode/src/backend/completion.ts
@@ -3,9 +3,19 @@ import * as Ano from "../parser/AnoParser";
 import { TerminalNode } from "antlr4ts/tree/TerminalNode";
 import { ParserRuleContext, Token } from "antlr4ts";
 import { CodeCompletionCore } from "antlr4-c3";
+import { ErrorNode } from "antlr4ts/tree/ErrorNode";
 
 function arr(txt: string) {
   return txt.split(",");
+}
+
+// Use completionproposals from grammar
+function getFromGrammar(ano: AnoHolder, index: number) {
+  const core = new CodeCompletionCore(ano.parser);
+  const candidates = core.collectCandidates(index);
+  return Array.from(candidates.tokens.keys()).map((i) =>
+    ano.parser.vocabulary.getLiteralName(i as number)?.replace(/'/gi, "")
+  ) as string[];
 }
 
 // Completions
@@ -14,61 +24,60 @@ export function getCompletion(
   char: number,
   key: string
 ): string[] {
-  const list: string[] = [];
   const ano = getActiveAno();
   if (ano.tokens.length === 1) {
     return ["table"];
   }
   const node = ano.findToken(line, char);
-  const previousRule = node ? ano.findRule(node.tokenIndex - 1) : null;
   const rule = node ? ano.findRule(node.tokenIndex) : null;
-  if (rule) list.push(...getSuggestions(previousRule, ano, node));
-  return list;
+  if (rule) {
+    const list = getSuggestions(rule, ano, node);
+    if (list.length) return list;
+  }
+  const prev = node ? ano.findRule(node.tokenIndex - 1) : null;
+  if (prev) {
+    const list = getSuggestions(prev, ano, node);
+    if (list.length) return list;
+  }
+  // Use completionproposals from grammar
+  const core = new CodeCompletionCore(ano.parser);
+  if (rule instanceof ErrorNode) {
+    const list = getFromGrammar(ano, (node?.tokenIndex as number) - 2);
+    const filtered = list.filter((x) => x && x.startsWith(key));
+    return filtered;
+  }
+  return getFromGrammar(ano, node?.tokenIndex as number);
 }
 
 function getSuggestions(context: any, ano: AnoHolder, node: Token | null): any {
-  const core = new CodeCompletionCore(ano.parser);
-  const candidates = core.collectCandidates(node?.tokenIndex as number);
   if (context === undefined) return [];
   if (context.constructor == TerminalNode) {
     return getSuggestions((<TerminalNode>context).parent, ano, node);
   }
   const ctx = <ParserRuleContext>context;
   switch (ctx.constructor) {
+    case TerminalNode:
+    case Ano.IdContext:
+      return getSuggestions(ctx.parent, ano, node);
     case Ano.ColumnContext:
       return arr(DATATYPES);
-    case Ano.TransformContext:
+    case Ano.TransformContext: // prev - before typing a char
+    case Ano.TransformprogContext: // rule - after typing a char
       return ano.getTransformationNames();
-    case Ano.ConvertContext:
+    case Ano.ConvertContext: // prev
+    case Ano.ConvertprogContext: // rule
       return ano.getConversionNames();
-    case Ano.DistributeContext:
+    case Ano.DistributeContext: // prev
+    case Ano.DistributeprogContext: // rule
       return ano.getDistributionNames();
     case Ano.MethodContext:
       return arr("cascading,not-in,not-exists");
-    case Ano.PropagateContext:
-    case Ano.CreateContext:
-    case Ano.CreateTableContext:
-    case Ano.UpdateContext:
-    case Ano.DeleteTableContext:
-    case Ano.EraseContext:
-    case Ano.EraseTableContext:
-    case Ano.SarContext:
-    case Ano.SarTableContext:
-    case Ano.DeleteContext:
+    case Ano.TableidContext:
       return ano.getTableNames();
-    case Ano.CreateChildColumnsContext:
-    case Ano.CreateParentColumnsContext:
-    case Ano.RandomizeContext:
-    case Ano.ShuffleContext:
-    case Ano.SelectionKeyContext:
-    case Ano.MaskContext:
     case Ano.SourceColumnContext:
-    case Ano.MaskColumnContext:
+    case Ano.ColumnidContext:
       return ano.getColumnNames(ano.getTableDef(ano.getTableName(ctx)));
-    default: {
-      return Array.from(candidates.tokens.keys()).map((i) =>
-        ano.parser.vocabulary.getLiteralName(i as number)?.replace(/'/gi, "")
-      );
-    }
+    default:
+      return [];
   }
 }
