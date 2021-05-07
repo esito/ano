631b9dc231622bcb739961ac6554ab3bc3edb342
Per Digre <per@digre.com>
2021-05-07 13:51:37 +0200

    Update completion.ts

--- vscode/src/backend/completion.ts
+++ vscode/src/backend/completion.ts
@@ -1,13 +1,11 @@
 import { AnoHolder, getActiveAno, DATATYPES } from "./anoclass";
 import * as Ano from "../parser/AnoParser";
 import { TerminalNode } from "antlr4ts/tree/TerminalNode";
-import { ParserRuleContext, Token } from "antlr4ts";
+import { ParserRuleContext } from "antlr4ts";
 import { CodeCompletionCore } from "antlr4-c3";
 import { ErrorNode } from "antlr4ts/tree/ErrorNode";
 
-function arr(txt: string) {
-  return txt.split(",");
-}
+const arr = (txt: string) => txt.split(",");
 
 // Use completionproposals from grammar
 function getFromGrammar(ano: AnoHolder, index: number) {
@@ -26,44 +24,43 @@ export function getCompletion(
 ): string[] {
   const ano = getActiveAno();
   const node = ano.findToken(line, char);
-  console.log(key + " " + node?.tokenIndex);
-  const rule = node ? ano.findRule(node.tokenIndex) : null;
+  if (node == null) return [];
+  let ntoken = node.tokenIndex;
+  let rule = ano.findRule(ntoken);
+  const isError = rule instanceof ErrorNode;
+  if (isError) {
+    ntoken -= 2;
+    rule = ano.findRule(ntoken);
+  }
+  console.log(key + " " + ntoken);
   if (rule) {
-    const list = getSuggestions(rule, ano, node);
+    const list = filterFix(getSuggestions(rule, ano), key);
     if (list.length) return list;
   }
   const prev = node ? ano.findRule(node.tokenIndex - 1) : null;
-  if (prev) {
-    const list = getSuggestions(prev, ano, node);
+  if (prev && key) {
+    const list = filterFix(getSuggestions(prev, ano), key);
     if (list.length) return list;
   }
 
   // Use completionproposals from grammar
-  const core = new CodeCompletionCore(ano.parser);
-  if (rule instanceof ErrorNode) {
-    return fixHyphen(
-      getFromGrammar(ano, (node?.tokenIndex as number) - 2),
-      key
-    );
-  }
-  return fixHyphen(getFromGrammar(ano, node?.tokenIndex as number), key);
+  return filterFix(getFromGrammar(ano, ntoken), key);
 }
 
-function fixHyphen(list: string[], key: string) {
-  const i =  key.indexOf("-") + 1;
+function filterFix(list: string[], key: string) {
+  const i = key.indexOf("-") + 1;
   return list.filter((x) => x && x.startsWith(key)).map((x) => x.substring(i));
 }
 
-function getSuggestions(context: any, ano: AnoHolder, node: Token | null): any {
+function getSuggestions(context: any, ano: AnoHolder): any {
   if (context === undefined) return [];
   if (context.constructor == TerminalNode) {
-    return getSuggestions((<TerminalNode>context).parent, ano, node);
+    return getSuggestions((<TerminalNode>context).parent, ano);
   }
   const ctx = <ParserRuleContext>context;
   switch (ctx.constructor) {
-    case TerminalNode:
     case Ano.IdContext:
-      return getSuggestions(ctx.parent, ano, node);
+      return getSuggestions(ctx.parent, ano);
     case Ano.ColumnContext:
       return arr(DATATYPES);
     case Ano.TransformContext: // prev - before typing a char
